3f272052cc61417531c06711f7d46823a17f62c4
ppraveenait <ppraveenait@gmail.com>
2023-03-10 19:57:52 +0530

    initial project changes

---
+++ src/main/java/com/skilltracker/querypatternsvc/QuerypatternsvcApplication.java
@@ -0,0 +1,13 @@
+package com.skilltracker.querypatternsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerypatternsvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerypatternsvcApplication.class, args);
+	}
+
+}
